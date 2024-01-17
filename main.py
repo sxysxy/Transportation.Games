fa9f0af5b934a52450cf2965bdda247f517362bb
HfCloud <sxysxygm@gmail.com>
2024-01-18 04:39:05 +0800

    add

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import flask
+import markdown2
+import os
 
 app = flask.Flask(__name__)
+MAIN_PAGE_MARKDOWN_FILENAME = "./主页Markdown.md"
+
+class EasyFileCache:
+    CACHES = {}
+    
+    def __init__(self, source, timestamp, content) -> None:
+        self.source = source
+        self.timestamp = timestamp
+        self.content = content
 
 # 判断是否是移动端设备(Android/ios)访问
 def is_mobile():
@@ -12,6 +23,27 @@ def is_mobile():
 @app.route("/index")
 def index():
     return flask.render_template("index.html")
+
+
+def get_markdown_content():
+    def loadf():
+        with open(MAIN_PAGE_MARKDOWN_FILENAME, "r") as f:
+            content = markdown2.markdown(f.read())
+            
+        obj = EasyFileCache(MAIN_PAGE_MARKDOWN_FILENAME, int(os.path.getmtime(MAIN_PAGE_MARKDOWN_FILENAME)), content)
+        return obj
+    
+    if 'markdown_of_main_page' in EasyFileCache.CACHES:
+        if int(os.path.getmtime(MAIN_PAGE_MARKDOWN_FILENAME)) > EasyFileCache.CACHES['markdown_of_main_page'].timestamp:
+            EasyFileCache.CACHES['markdown_of_main_page'] = loadf()
+    else:
+        EasyFileCache.CACHES['markdown_of_main_page'] = loadf()
+    return EasyFileCache.CACHES['markdown_of_main_page'].content
+        
+
+@app.route("/markdown_of_main_page")
+def markdown_of_main_page():
+    return get_markdown_content()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=12345, debug=True)
---
+++ 主页Markdown.md
@@ -0,0 +1,3 @@
+# Transportation Game
+
+这里可以写Markdown，网页的主页就能显示出来！
